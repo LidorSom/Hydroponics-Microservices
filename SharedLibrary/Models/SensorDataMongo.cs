6762133b5bdc92fc957c7eed203bc13e0b942643
Lidor Sommer <lidorso@mta.ac.il>
2024-07-26 16:15:36 +0300

    Finished seperation of writing and reading from mongoDB the sensors data

---
+++ SharedLibrary/Models/SensorDataMongo.cs
@@ -0,0 +1,11 @@
+﻿namespace SharedLibrary;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+public class SensorDataMongo : SensorDataBase
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.Binary)]
+    [BsonGuidRepresentation(GuidRepresentation.Standard)]
+    public new Guid Id { get; set; } = Guid.NewGuid();
+}
--- SharedLibrary/obj/Debug/net8.0/SharedLibrary.AssemblyInfo.cs
+++ SharedLibrary/obj/Debug/net8.0/SharedLibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LidorSo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da0b44cd57b2236becb4bf39ef4a78e7f8947570")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7ffda39375625d77e9e40e945f1ffbfef52b533")]
 [assembly: System.Reflection.AssemblyProductAttribute("SharedLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SharedLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
