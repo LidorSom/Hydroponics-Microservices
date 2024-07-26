d7ffda39375625d77e9e40e945f1ffbfef52b533
Lidor Sommer <lidorso@mta.ac.il>
2024-07-26 16:00:27 +0300

    Made shared lib for mutual Models, made as a nuget package

---
+++ SharedLibrary/Models/SensorDataBase.cs
@@ -0,0 +1,13 @@
+﻿namespace SharedLibrary;
+public class SensorDataBase
+{
+    /// <summary>
+    /// Id - the Id per measuring system of a user (user can have many measuing systems)
+    /// </summary>
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public double Ph { get; set; }
+    public double Ec { get; set; }
+    public double Temperature { get; set; }
+    public double Humidity { get; set; }
+    public DateTime Timestamp { get; set; }
+}
